9acbdaa42ffe2700f4c087017d615a04a9d7fa24
Papa Amadou GNING <papagning4@gmail.com>
2023-04-30 15:43:51 +0000

    Checkpoint API

---
+++ src/UserList.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+
+function UserList() {
+const [listOfUsers, setListOfUsers] = useState([]);
+
+useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/users')
+    .then(response => {
+        setListOfUsers(response.data);
+    })
+    .catch(error => {
+        console.log(error);
+    });
+}, []);
+
+return (
+    <div >
+        <h3>Liste des Utilisateurs</h3>
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nom</th>
+                    <th>Adresse</th>
+                    <th>Email</th>
+                    <th>Téléphone</th>
+                    <th>Site Web</th>
+                </tr>
+            </thead>
+            <tbody>
+                {listOfUsers.map(user => (
+                    <tr>
+                        <td className='idRow'>{user.id}</td>
+                        <td>{user.name}</td>
+                        <td>{user.address.street}</td>
+                        <td>{user.email}</td>
+                        <td>{user.phone}</td>
+                        <td>{user.website}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    </div>
+);
+}
+
+export default UserList;
